949e328bb5abf4d50f1c7700ac93c7c81ebfdcad
kevinRetalapp <kevin.steven@retalapp.co>
2021-10-25 12:01:56 -0500

    Header complete

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/themes" component={}/>
         <Route path="/agency" component={}/> */}
       </Switch>
-      {/* <Footer/> */}
+      <Footer/>
     </Router>
   );
 }
--- src/components/HeaderSection/index.jsx
+++ src/components/HeaderSection/index.jsx
@@ -4,10 +4,27 @@ import Divs from '../../assets/Divs.png'
 const HeaderSection = () => {
     return (
         <div className="header_container">
-           
+
             <div className="header_items">
                 <h1 className="header_title">More than a css framework</h1>
                 <button className="btn">Get Started</button>
+                <div className="header_features">
+                    <div >
+                        <div className="header_circle purple" />
+                        <span>Fast</span>
+
+                    </div>
+                    <div>
+                        <div className="header_circle orange" />
+                        <span>Custom</span>
+                    </div>
+
+                    <div>
+                        <div className="header_circle red" />
+                        <span>Responsive</span>
+                    </div>
+
+                </div>
 
             </div>
             <div className="header_image_container">
